e74495ce73bf0afe9f5a64c6e4f963c8855b9c6d
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-05-02 10:38:04 +0200

    Add Product component content'

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,8 +1,25 @@
 import React from 'react';
 
-function Product() {
+function Product({ product }) {
+  const {
+    name, image, price, rating,
+  } = product;
+
   return (
-    <div>Product</div>
+    <div key={product.description} className="card col-md-3 m-3 p-2">
+      <img src={image} alt="product" className="product-img" />
+      <h1 className="card-text">{name}</h1>
+      <p className="card-text">
+        <b>Price:</b>
+        {' '}
+        {price}
+      </p>
+      <p className="card-text">
+        <b>Rating:</b>
+        {' '}
+        {rating}
+      </p>
+    </div>
   );
 }
 
