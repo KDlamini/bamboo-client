d09f33b74b50a70aa92b645314a41be3b5f3debc
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-05-02 17:33:07 +0200

    Update product component

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Rating from 'react-rating';
 
 function Product({ product }) {
   const {
@@ -8,9 +9,9 @@ function Product({ product }) {
 
   return (
     <div key={product.description} className="card col-md-3 m-3 p-2">
-      <Link to={`product/${id}`}>
+      <Link to={`product/${id}`} className="link text-dark">
         <img src={image} alt="product" className="product-img" />
-        <h1 className="card-text">{name}</h1>
+        <h1 className="card-text name">{name}</h1>
         <p className="card-text">
           <b>Price:</b>
           {' '}
@@ -19,7 +20,11 @@ function Product({ product }) {
         <p className="card-text">
           <b>Rating:</b>
           {' '}
-          {rating}
+          <Rating
+            initialRating={rating}
+            emptySymbol="fa-regular fa-star me-2"
+            fullSymbol="fa-solid fa-star me-2"
+          />
         </p>
       </Link>
     </div>
