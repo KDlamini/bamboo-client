0124318cc9bb2a5bee0ee0c71dae7bf38da59207
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-05-02 12:10:19 +0200

    Add ProductDetails route Link on each Product

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,24 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Product({ product }) {
   const {
-    name, image, price, rating,
+    id, name, image, price, rating,
   } = product;
 
   return (
     <div key={product.description} className="card col-md-3 m-3 p-2">
-      <img src={image} alt="product" className="product-img" />
-      <h1 className="card-text">{name}</h1>
-      <p className="card-text">
-        <b>Price:</b>
-        {' '}
-        {price}
-      </p>
-      <p className="card-text">
-        <b>Rating:</b>
-        {' '}
-        {rating}
-      </p>
+      <Link to={`product/${id}`}>
+        <img src={image} alt="product" className="product-img" />
+        <h1 className="card-text">{name}</h1>
+        <p className="card-text">
+          <b>Price:</b>
+          {' '}
+          {price}
+        </p>
+        <p className="card-text">
+          <b>Rating:</b>
+          {' '}
+          {rating}
+        </p>
+      </Link>
     </div>
   );
 }
