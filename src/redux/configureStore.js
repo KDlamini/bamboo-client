ccff6ef4f536f30044aa60d88af0ff2a1a6e0ac0
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-05-17 17:17:22 +0200

    Connect redux logger middleware

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
+import logger from 'redux-logger';
 import products from './reducers/products/products';
 
 const store = configureStore({
   reducer: {
     products,
   },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 export default store;
