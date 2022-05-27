3c18b09bfb47403e5d405b85a74d0126c8ed640d
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-05-27 12:41:16 +0200

    Add ErrorBoundary

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
 import ProductDetails from './pages/ProductDetails';
+import ErrorBoundary from './components/ErrorBoundary';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
@@ -9,10 +10,17 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <Routes>
-        <Route exact path="/" element={<Homepage />} />
-        <Route path="/product/:id" element={<ProductDetails />} />
-      </Routes>
+      <ErrorBoundary>
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route
+            path="/product/:id"
+            element={(
+              <ProductDetails />
+          )}
+          />
+        </Routes>
+      </ErrorBoundary>
     </div>
   );
 }
