b85cc30006acad9d2251ff256212030ab5423cf7
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-06-03 14:25:18 +0200

    Update Homepage layout and styling

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import Product from '../components/Product';
+import Filters from '../components/Filters';
 import { getProducts } from '../redux/actions/products';
 
 function Homepage() {
-  const products = useSelector((state) => state.products);
+  const selectProducts = useSelector((state) => state.products);
+  const [products, setProducts] = useState(selectProducts);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -14,18 +16,25 @@ function Homepage() {
   }, []);
 
   return (
-    <section className="container-fluid m-0 d-flex justify-content-center">
-      <div className="container row justify-content-center">
-        {
-          products && products.map((product) => {
-            const { _id: id } = product;
+    <section className="container-fluid m-0">
+      <div className="row">
+        <div className="col-md-3 p-0">
+          <Filters setProducts={setProducts} />
+        </div>
+        <div className="col-md-9">
+          <div className="products detail-box d-flex py-2">
+            {
+              products && products.map((product) => {
+                const { _id: id } = product;
 
-            return (
-              <Product key={id} product={product} />
-            );
-          })
+                return (
+                  <Product key={id} product={product} />
+                );
+              })
 
-        }
+            }
+          </div>
+        </div>
       </div>
     </section>
   );
