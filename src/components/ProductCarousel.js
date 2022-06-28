83f050e51b594f36329bc8aaddb0f46bb8defaa0
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-06-28 22:12:40 +0200

    Upadate Slide to show per scroll

--- src/components/ProductCarousel.js
+++ src/components/ProductCarousel.js
@@ -12,13 +12,13 @@ const ProductCarousel = ({ renderProducts, heading }) => {
     swipeToSlide: true,
     speed: 500,
     slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToScroll: 1,
     responsive: [
       {
         breakpoint: 1024,
         settings: {
           slidesToShow: 3,
-          slidesToScroll: 3,
+          slidesToScroll: 1,
           infinite: true,
         },
       },
@@ -26,7 +26,7 @@ const ProductCarousel = ({ renderProducts, heading }) => {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
+          slidesToScroll: 1,
           initialSlide: 2,
         },
       },
