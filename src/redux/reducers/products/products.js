4b9ddf51aa6cc4af0a642b730bc9609aef2f22a7
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-06-28 18:56:34 +0200

    Calculate discount price and save new state

--- src/redux/reducers/products/products.js
+++ src/redux/reducers/products/products.js
@@ -3,7 +3,18 @@ import { FETCH_ALL_PRODUCTS } from '../../actions/actionTypes';
 const productsReducer = (state = [], action) => {
   switch (action.type) {
     case FETCH_ALL_PRODUCTS:
-      return action.payload;
+      return action.payload.map((product) => {
+        const { deals, price } = product;
+        const { discount, available } = deals[0];
+
+        if (available) {
+          return {
+            ...product,
+            discountPrice: Math.round(((100 - discount) / 100) * price),
+          };
+        }
+        return product;
+      });
 
     default:
       return state;
