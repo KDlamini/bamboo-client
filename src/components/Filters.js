713c438f748638f1a7cfa7f94fc51dde447f6c14
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-06-29 18:26:56 +0200

    Update Filters component layout

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
 
 function Filters() {
   return (
     <div>
-      <div className="detail-box filter-box text-start ms-3 mt-2 p-2 border rounded-1">
-        <h3 className="title pb-2 border-bottom">Refine by Category</h3>
-        <NavLink to="/query" className="text-start details-link">Search Page</NavLink>
+      <div className="detail-box filter-box text-start ms-3 mt-2 border rounded-1">
+        <h3 className="title text-light bg-dark p-2 border-bottom">Refine by Category</h3>
       </div>
-      <div className="detail-box text-start ms-3 my-2 p-2 border rounded-1">
-        <h3 className="title pb-2 border-bottom">Filters</h3>
+      <div className="detail-box text-start ms-3 my-2 border rounded-1">
+        <h3 className="title text-light bg-dark p-2 border-bottom">Filters</h3>
         <div className="filter-box">
-          <h4 className="details-info-text">Price</h4>
+          <h4 className="details-info-text px-2">Price</h4>
         </div>
         <div className="filter-box border-top">
-          <h4 className="details-info-text py-2">Rating</h4>
+          <h4 className="details-info-text p-2">Rating</h4>
         </div>
       </div>
     </div>
