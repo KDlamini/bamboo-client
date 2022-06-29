fbb4c920b696830edd1f3a27efb33169f1571fc6
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-06-29 13:29:23 +0200

    Create Long Banner component for wider Ads

--- src/components/advertisements.js
+++ src/components/advertisements.js
@@ -3,6 +3,12 @@ import appleWatches2 from '../assets/bannerAd2.png';
 import bombasShocks from '../assets/bannerAd1.gif';
 import sale from '../assets/sale1.gif';
 import cyclingAd from '../assets/bannerAd3.gif';
+import longAd from '../assets/longAd.png';
+import longAd1 from '../assets/longAd1.png';
+import longAd3 from '../assets/longAd3.png';
+import longAd4 from '../assets/longAd4.png';
+import longAd5 from '../assets/longAd5.png';
+import longAd6 from '../assets/longAd6.png';
 
 const data = [
   {
@@ -23,6 +29,33 @@ const data = [
   },
   {
     image: sale,
+    url: '',
+  },
+];
+
+export const longAdverts = [
+  {
+    image: longAd,
+    url: '',
+  },
+  {
+    image: longAd1,
+    url: '',
+  },
+  {
+    image: longAd3,
+    url: '',
+  },
+  {
+    image: longAd4,
+    url: '',
+  },
+  {
+    image: longAd5,
+    url: '',
+  },
+  {
+    image: longAd6,
     url: '',
   },
 ];
