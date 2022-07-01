5bf76151384fc94c920fe44d818e9c3390600e62
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-01 10:55:29 +0200

    Rename data file

--- src/components/Departments.js
+++ src/components/Departments.js
@@ -3,21 +3,11 @@ import { NavLink, useNavigate } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { DropdownButton, ButtonGroup, Dropdown } from 'react-bootstrap';
 import { getPromotions, filterDepartment } from '../redux/actions/products';
+import { departmentsList } from './data';
 
 const Departments = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-
-  const departmentsList = {
-    Beauty: ['Men\'s Grooming', 'Fragrances', 'Hair Care', 'Makeup', 'Skin Care', 'Body Care'],
-    'Computers & Electronics': ['Desktop', 'Monitors', 'Computer Accessories', 'Laptop', 'Laptop Accessories', 'Software', 'Photography', 'Drones'],
-    Gadgets: ['Smartphones', 'Tablets', 'Smartwatches', 'Smart Accessories'],
-    Fashion: ['Men', 'Women', 'Kids', 'Watches', 'Jewellery', 'Footwear', 'Backpacks'],
-    Gaming: ['Gaming Accessories', 'Playstation', 'Nintendo', 'Xbox Series'],
-    'TV, Audio & Media': ['TV\'s', 'Home Entertainment', 'Headsets', 'Microphones', 'Guitars', 'Keyboard & Piano'],
-    'Home & Appliances': ['Appliances', 'Kitchen', 'Furniture & Decor', 'Bed & Bath'],
-    Sports: ['Sports Clothing', 'Sports Footwear', 'Sports Wearable Tech', 'Sports Nutrition', 'Cycling', 'Gym Equipment'],
-  };
 
   const departmentKeys = Object.keys(departmentsList);
 
