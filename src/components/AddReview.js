ffdbae7555500638b7cedf5d9938b3386d12908c
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-06 00:00:08 +0200

    Add state management for review commnets

--- src/components/AddReview.js
+++ src/components/AddReview.js
@@ -1,40 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Rating from 'react-rating';
 import { Form, Button } from 'react-bootstrap';
 
-const AddReview = () => (
-  <div className="py-2">
-    <Form>
-      <Form.Group className="mb-3">
-        <Form.Label className="description-text me-2">Rate: </Form.Label>
-        <Rating
-          initialRating={0}
-          emptySymbol="fa-regular fa-star me-1"
-          fullSymbol="fa-solid fa-star me-1"
-          className="text-warning"
-        />
-      </Form.Group>
+const AddReview = () => {
+  const [rating, setRating] = useState(0);
+  const [comment, setComment] = useState('');
 
-      <Form.Group className="mb-3">
-        <Form.Control
-          className="comment-text"
-          as="textarea"
-          rows="3"
-          name="comment"
-          placeholder="Write review comment"
-        />
-      </Form.Group>
+  const handleSubmit = (e) => {
+    e.preventDefault();
 
-      <div className="actions text-end">
-        <Button variant="primary" type="submit" className="buy btn-secondary rounded-1 me-3">
-          Cancel
-        </Button>
-        <Button variant="primary" type="submit" className="buy btn-success rounded-1">
-          Submit
-        </Button>
-      </div>
-    </Form>
-  </div>
-);
+    const review = {
+      rating,
+      comment,
+      createdAt: new Date(),
+      user: {
+        name: 'John Doe',
+        avatar: 'https://randomuser.me/api/portraits/',
+      },
+    };
+
+    return review;
+  };
+
+  return (
+    <div className="py-2">
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label className="description-text me-2">Rate: </Form.Label>
+          <Rating
+            initialRating={0}
+            emptySymbol="fa-regular fa-star me-1"
+            fullSymbol="fa-solid fa-star me-1"
+            className="text-warning"
+            onChange={(rating) => setRating(rating)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3">
+          <Form.Control
+            className="comment-text"
+            as="textarea"
+            rows="5"
+            name="comment"
+            placeholder="Write review comment"
+            onChange={(e) => setComment(e.target.value)}
+          />
+        </Form.Group>
+
+        <div className="actions text-end">
+          <Button variant="primary" type="submit" className="buy btn-secondary rounded-1 me-3">
+            Cancel
+          </Button>
+          <Button variant="primary" type="submit" className="buy btn-success rounded-1">
+            Submit
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
 
 export default AddReview;
