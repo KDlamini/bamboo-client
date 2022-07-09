6d8271d8feed954bd3a2191f30e56ed1ff587620
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-09 16:31:34 +0200

    Add user auth and errors reducers to Redux store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,11 +2,15 @@ import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import products from './reducers/products/products';
 import cart from './reducers/cart/cart';
+import error from './reducers/users/errors';
+import auth from './reducers/users/auth';
 
 const store = configureStore({
   reducer: {
     products,
     cart,
+    auth,
+    error,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
