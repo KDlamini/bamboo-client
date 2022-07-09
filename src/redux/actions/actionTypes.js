e49a63cee9e7603d16c66ce7ee218ff0152c2c4d
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-09 12:22:01 +0200

    Add new action types for authentication

--- src/redux/actions/actionTypes.js
+++ src/redux/actions/actionTypes.js
@@ -11,3 +11,14 @@ export const QUERY_BY_DEPARTMENT = 'Shopcart/api/QUERY_BY_DEPARTMENT';
 export const FETCH_ALL_PROMOTIONS = 'Shopcart/api/FETCH_ALL_PROMOTIONS';
 
 export const ADD_REVIEW = 'Shopcart/api/ADD_REVIEW';
+
+export const USER_LOADING = 'Shopcart/api/USER_LOADING';
+export const USER_LOADED = 'Shopcart/api/USER_LOADED';
+export const AUTH_ERROR = 'Shopcart/api/AUTH_ERROR';
+export const LOGIN_SUCCESS = 'Shopcart/api/LOGIN_SUCCESS';
+export const LOGIN_FAIL = 'Shopcart/api/LOGIN_FAIL';
+export const LOGOUT_SUCCESS = 'Shopcart/api/LOGOUT_SUCCESS';
+export const REGISTER_SUCCESS = 'Shopcart/api/REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'Shopcart/api/REGISTER_FAIL';
+export const GET_ERRORS = 'Shopcart/api/GET_ERRORS';
+export const CLEAR_ERRORS = 'Shopcart/api/CLEAR_ERRORS';
