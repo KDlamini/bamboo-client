0bd5d7d5835dd60dcee2dab76f5e0b5aa6a618ab
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-07-11 13:17:28 +0200

    Update Login UI

--- src/components/auth/LoginModal.js
+++ src/components/auth/LoginModal.js
@@ -6,6 +6,7 @@ import {
 import { connect } from 'react-redux';
 import { login } from '../../redux/actions/users';
 import { clearErrors } from '../../redux/actions/errors';
+import loginImage from '../../assets/ModernCabinet.gif';
 
 const LoginModal = ({
   isAuthenticated, error, login, clearErrors,
@@ -48,42 +49,53 @@ const LoginModal = ({
         Login
       </NavLink>
 
-      <Modal isOpen={modal} toggle={handleToggle}>
-        <ModalHeader toggle={handleToggle}>Login</ModalHeader>
-        <ModalBody>
-          {message ? <Alert color="danger">{message}</Alert> : null}
-          <Form>
-            <FormGroup>
-              <Label for="email">Email</Label>
-              <Input
-                type="email"
-                name="email"
-                id="email"
-                placeholder="Email"
-                className="mb-3"
-                onChange={(e) => setEmail(e.target.value)}
-              />
+      <Modal isOpen={modal} toggle={handleToggle} className="modal-lg border-0">
+        <div className="row popup-container border border-success rounded-1">
+          <div className="col-md-5 popup d-flex align-items-center p-0 rounded-1">
+            <div className="modal-form-wrapper bg-light w-100 rounded-2">
+              <ModalHeader toggle={handleToggle}>Login</ModalHeader>
+              <ModalBody>
+                {message ? <Alert color="danger">{message}</Alert> : null}
+                <Form>
+                  <FormGroup>
+                    <Label for="email">Email</Label>
+                    <Input
+                      type="email"
+                      name="email"
+                      id="email"
+                      placeholder="Email"
+                      className="mb-3"
+                      onChange={(e) => setEmail(e.target.value)}
+                    />
 
-              <Label for="password">Password</Label>
-              <Input
-                type="password"
-                name="password"
-                id="password"
-                placeholder="Password"
-                className="mb-3"
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <Button
-                color="dark"
-                style={{ marginTop: '2rem' }}
-                block
-                onClick={handleOnSubmit}
-              >
-                Login
-              </Button>
-            </FormGroup>
-          </Form>
-        </ModalBody>
+                    <Label for="password">Password</Label>
+                    <Input
+                      type="password"
+                      name="password"
+                      id="password"
+                      placeholder="Password"
+                      className="mb-3"
+                      onChange={(e) => setPassword(e.target.value)}
+                    />
+                    <Button
+                      color="dark"
+                      style={{ marginTop: '2rem' }}
+                      block
+                      onClick={handleOnSubmit}
+                    >
+                      Login
+                    </Button>
+                  </FormGroup>
+                </Form>
+              </ModalBody>
+            </div>
+          </div>
+          <div className="col-md-7 d-flex align-items-center popup p-0 rounded-1">
+            <div className="details-img-wrapper rounded-1">
+              <img src={loginImage} alt="login-banner" className="details-img rounded-1" />
+            </div>
+          </div>
+        </div>
       </Modal>
     </div>
   );
