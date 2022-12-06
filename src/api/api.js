2ff81a0c41784fc407adf47e36feb8ceaacbd7f8
Simo Nkosi <wilzmar.kwd@gmail.com>
2022-12-06 11:41:14 +0200

    Update API url

--- src/api/api.js
+++ src/api/api.js
@@ -1,4 +1,4 @@
-const url = 'http://localhost:5000';
+const url = 'https://shopcart-server.vercel.app/';
 const geoLocationUrl = 'https://api.ipgeolocation.io/ipgeo?apiKey=24f6c248269d4012ac782238eb67dfe9';
 
 const getData = async (url) => {
